1ba801066401d8de841644beb38cc8021d9334bd
jamesrx13 <rudasmarinjf@gmail.com>
2025-07-30 21:24:40 -0500

    Agregar componentes de usuarios y formularios, actualizar estilos y rutas, e incluir imágenes

--- src/app/pages/forms/forms.component.ts
+++ src/app/pages/forms/forms.component.ts
@@ -1,14 +1,15 @@
-import { NgClass, NgIf } from '@angular/common';
+import { CommonModule, NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FluidModule } from 'primeng/fluid';
 import { InputTextModule } from 'primeng/inputtext';
 import { DatePickerModule } from 'primeng/datepicker';
 import { SelectModule } from 'primeng/select';
 import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-forms',
-  imports: [NgIf, NgClass, FluidModule, InputTextModule, DatePickerModule, SelectModule, ButtonModule],
+  imports: [NgIf, NgClass, FluidModule, InputTextModule, DatePickerModule, SelectModule, ButtonModule, CommonModule, RouterModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ButtonModule } from 'primeng/button';
+import { DatePickerModule } from 'primeng/datepicker';
+import { DialogModule } from 'primeng/dialog';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [
+    BreadcrumbModule,
+    ButtonModule,
+    TableModule,
+    DialogModule,
+    InputTextModule,
+    FloatLabelModule,
+    DatePickerModule,
+    BadgeModule,
+    SelectModule,
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
+
+  items: MenuItem[] | undefined;
+
+  home: MenuItem | undefined;
+
+  visible: boolean = false;
+
+  modalHeader: string = 'Nuevo Usuario';
+
+  roles: string[] = [
+    'Admin',
+    'User'
+  ]
+
+  ngOnInit() {
+    this.home = { icon: 'pi pi-home', routerLink: '/dashboard' };
+    this.items = [{ label: 'Usuarios' }];
+  }
+
+  selectModalMode(edit: boolean = false) {
+    if (edit) {
+      this.modalHeader = 'Editar Usuario';
+      // Cargar el modelo
+    } else {
+      this.modalHeader = 'Nuevo Usuario';
+      // Limpiar el modelo
+    }
+    this.visible = true;
+  }
 
 }
